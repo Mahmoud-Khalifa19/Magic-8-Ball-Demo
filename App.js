0e71e28f7a7fcf304a6ddf45a0beb0770fbd7581
mariamjaludi <mariam.jaludi@gmail.com>
2019-09-17 22:50:51 -0700

    tests

--- App.js
+++ App.js
@@ -4,10 +4,40 @@ import {getResponse} from './responses';
 import style from './styles'
 
 const App = () => {
+    const [text, setState] = useState('')
     return (
+      < >
         <View style={style.titleView}>
-            <Text style={style.title}>Maigc 8 Ball</Text>
+            <Text style={style.title}>Magic 8 Ball</Text>
         </View>
+        <ImageBackground
+          style={style.imageBackground}
+          testID="image-background"
+          source={require('./assets/triangle-1.png')}
+        >
+          <View
+            style={style.textView}
+          >
+            <Button
+              title="Click Me"
+              color="black"
+              onPress={() => {
+                setState(getResponse())
+              }}
+              testID="button"
+            >
+
+            </Button>
+            <Text
+              style = {style.response}
+              testID = "response-text"
+              >
+              {text}
+            </Text>
+          </View>
+
+        </ImageBackground>
+      </>
     );
 }
 
--- __tests__/components/App.spec.js
+++ __tests__/components/App.spec.js
@@ -1,13 +1,44 @@
 import React from 'react';
-import { render } from 'react-native-testing-library';
+import { fireEvent, render } from 'react-native-testing-library';
 import App from '../../App';
 import * as response from '../../responses';
 import { toBeEmpty, toHaveTextContent } from '@testing-library/jest-native';
 expect.extend({ toBeEmpty, toHaveTextContent });
 
 describe('App', () => {
-    it('renders the correct title', () => {
-        const { queryByText } = render(<App />);
-        expect(queryByText('Maigc 8 Ball')).not.toBeNull();
-    });
+  it('renders the correct title', () => {
+      const { queryByText } = render(<App />);
+      expect(queryByText('Magic 8 Ball')).not.toBeNull();
+  });
+
+  it('renders the correct button title', () => {
+      const { queryByTestId, queryByText } = render(<App />);
+      expect(queryByTestId('button')).not.toBeNull();
+      expect(queryByText('Click Me')).not.toBeNull();
+  });
+
+  it('renders the correct image background', () => {
+      const { queryByTestId } = render(<App />);
+      expect(queryByTestId('image-background')).not.toBeNull();
+      // expect(queryByText('Click Me')).not.toBeNull();
+  });
+
+
+  it('renders a response with an empty string', () => {
+      const { queryByTestId} = render(<App />);
+      const responseText = queryByTestId("response-text")
+      expect(responseText).not.toBeNull();
+      expect(responseText).toHaveTextContent("")
+  });
+
+  it('when the button is clicked, get response is called', () => {
+    response.getResponse = jest.fn().mockImplementationOnce(() => {return "N"})
+      const { queryByTestId } = render(<App />);
+      const button = queryByTestId('button')
+      const responseText = queryByTestId("response-text")
+      fireEvent.press(button)
+      expect(response.getResponse).toHaveBeenCalled();
+      expect(responseText).toHaveTextContent("N");
+  });
+
 });
